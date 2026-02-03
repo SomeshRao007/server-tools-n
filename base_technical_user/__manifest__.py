9188523c36d08d4d84f1bd85e1489450d7461861
Somesh Rao Coka <111784343+SomeshRao007@users.noreply.github.com>
2026-02-03 18:18:55 +0530

    Update __manifest__.py

--- base_technical_user/__manifest__.py
+++ base_technical_user/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-tools",
     "category": "Hidden/Dependency",
-    "version": "19.0.10.0.0",
+    "version": "19.0.11.0.0",
     "license": "AGPL-3",
     "development_status": "Production/Stable",
     "depends": ["base"],
