f06a15112cbc4eb7001888b812820fe740262819
Somesh Rao Coka <111784343+SomeshRao007@users.noreply.github.com>
2026-03-12 17:04:33 +0530

    Update version to 18.0.1.3.2

--- base_cron_exclusion/__manifest__.py
+++ base_cron_exclusion/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Cron Exclusion",
     "summary": "Allow you to select scheduled actions that should not run "
     "simultaneously.",
-    "version": "18.0.1.2.9",
+    "version": "18.0.1.3.2",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "maintainers": ["LoisRForgeFlow", "ChrisOForgeFlow"],
     "development_status": "Production/Stable",
